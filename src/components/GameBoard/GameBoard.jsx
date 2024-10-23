19cfd87aa0d7ba46b4ceb8bab4b3577cfd83a4a2
eternxl-decxdence <sonicsoliar@gmail.com>
2024-10-23 12:10:42 +0200

    finished move logic, promotion not implemented

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,30 +1,56 @@
 import { Chess } from "chess.js";
 import { useState } from "react";
 import { DndContext } from "@dnd-kit/core";
+import { defaultSquareNotation} from "../../utils/js/utils";
 
-import Piece from "../Piece/Piece";
 
 import "./GameBoard.scss";
 import "./Coordinates.scss";
-const chess = new Chess();
+import Square from "../Square/Square";
+import PromotionDialog from "../PromotionDialog/PromotionDialog";
+
+
+//const chess = new Chess();
+
+
+const chess = new Chess("1K6/P7/8/8/8/8/7p/6k1 w - - 0 1");
 
 export default function GameBoard() {
   const [chessboard, setChessboard] = useState(chess.board());
-
-  function onSquareSelection(square) {}
-  function onMove(from, to) {}
-
+  const [activeSquare, setActiveSquare] = useState(null);
+  const [promotion, setPromotion] = useState({active: false, moveTo: null});
+  function onSquareSelection(square) {
+    console.log()
+    setActiveSquare(square);
+  }
+  function onMove(to) {
+    chess.move({from: activeSquare.square, to: to });
+    setChessboard(chess.board());
+  }
+  function onPromotion(to){
+    setPromotion({active: true, moveTo: to});
+  }
   return (
     <DndContext>
       <div className='board'>
         {chessboard.map((row, rowIndex) =>
           row.map((square, colIndex) => (
-            <div key={`${colIndex}${rowIndex}`} className='square'>
-              {square && <Piece pieceData={square} />}
-            </div>
+           <Square 
+            position={{col: colIndex, row: rowIndex}}
+            key={`${colIndex}${rowIndex}`} 
+            squareData={square ? square : {square: defaultSquareNotation(colIndex,rowIndex)}}
+            activeSquare={activeSquare}
+            isPossibleMove={activeSquare !== null && chess.moves({square: activeSquare.square, verbose: true}).map(({to}) => to).includes(defaultSquareNotation(colIndex,rowIndex))}
+            onSquareSelect={onSquareSelection}
+            onMove={onMove}/>
           ))
         )}
       </div>
+      {promotion.active && 
+      <PromotionDialog
+        activeSquare={activeSquare}
+        moveTo={promotion.moveTo}
+      />}
     </DndContext>
   );
 }
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,94 +1,26 @@
+import { PAWN, WHITE, BLACK } from "chess.js";
+import Piece from "../Piece/Piece";
 import "./Square.scss";
-import {
-  columnIndexes,
-  defaultSquareNotation,
-  returnCornerClass
-} from "../../utils/js/utils";
-
-import { WHITE, BLACK, PAWN, KING } from "chess.js";
-
 export default function Square({
-  activeSquareData,
-  squareData,
-  children,
-  row,
-  column,
-  chess,
-  onPieceSelect,
-  isPossibleMove,
-  isLastMove,
-  onPieceMove,
-  onPromotion
-}) {
-  function handleSquareClick() {
-    if (squareData != null && squareData.color === chess.turn()) {
-      onPieceSelect(squareData);
-    }
-  }
-  const squareClassName = `square ${
-    (column % 2 == 0 && row % 2 == 0) || (column % 2 != 0 && row % 2 !== 0)
-      ? "square-light"
-      : "square-dark"
-  }${isLastMove ? " last-move" : ""}${
-    isPossibleMove && squareData != null ? " attack" : ""
-  }${
-    (row == 0 && column == 0) ||
-    (row == 0 && column == 7) ||
-    (row == 7 && column == 0) ||
-    (row == 7 && column == 7)
-      ? " corner-" + returnCornerClass(row, column)
-      : ""
-  }${
-    chess.inCheck() &&
-    squareData != null &&
-    squareData.type == KING &&
-    squareData.color == chess.turn()
-      ? " checked"
-      : ""
-  }`;
-
-  const rowCoordinate =
-    row % 2 == 0 ? "coordinate-row-light" : "coordinate-row-dark";
-  const columnCordinate =
-    column % 2 == 0 ? "coordinate-col-dark" : "coordinate-col-light";
-  const possibleMoveClassName = `possible-move${
-    squareData != null ? "-attack" : "-empty"
-  }`;
-
-  function handlePossibleMoveClick(event) {
-    if (
-      activeSquareData.type == PAWN &&
-      ((activeSquareData.color == WHITE && row == 0) ||
-        (activeSquareData.color == BLACK && row == 7))
-    ) {
-      onPromotion(row, column);
-    } else {
-      onPieceMove(defaultSquareNotation(column, row));
-    }
-  }
-
+  position,
+  squareData, 
+  isPossibleMove, 
+  onSquareSelect,
+  onPromotion,
+  onMove}) {
+  const possibleMoveClick = 
+    (squareData == {type: PAWN, color: WHITE} && position.row == 0) ||
+    (squareData == {type: PAWN, color: BLACK} && position.row == 7) ?
+      ( () => onPromotion(squareData.square)) :
+      ( () => onMove(squareData.square))
   return (
-    <div
-      key={`${columnIndexes[column]}${row}`}
-      className={squareClassName}
-      onClick={handleSquareClick}
-    >
-      {column == 0 ? (
-        <span className={rowCoordinate}>{(row - 8) * -1}</span>
-      ) : null}
-
-      {isPossibleMove ? (
-        <div
-          className={possibleMoveClassName}
-          onClick={handlePossibleMoveClick}
-        ></div>
-      ) : null}
-
-      {children}
-
-      {row == 7 ? (
-        <span className={columnCordinate}>{columnIndexes[column]}</span>
-      ) : null}
+    <div 
+      onClick={squareData && (() => onSquareSelect(squareData))}
+      className='square'>
+        {squareData.type && <Piece pieceData={squareData} />}
+        {isPossibleMove && 
+          <div onClick={possibleMoveClick}
+          className={`possible-move${squareData.type ? '-attack' : ""}`}></div>}
     </div>
-  );
+  )
 }
